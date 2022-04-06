bbd711b06690f27ad8d205416fd27a44b857ddcb
Aime Malaika <aimemalaika1995@gmail.com>
2022-04-06 18:31:52 +0200

    add àttribute accessors Person

--- person.rb
+++ person.rb
@@ -1,4 +1,7 @@
 class Person
+  attr_reader :id, :parent_permission
+  attr_accessor :age, :name
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1...10_000)
     @name = name
@@ -9,7 +12,7 @@ class Person
   private
 
   def of_age?
-    return true if @age >= 18
+    return true if age >= 18
 
     false
   end
@@ -17,7 +20,7 @@ class Person
   public
 
   def can_use_services?
-    return true if of_age? || @parent_permission
+    return true if of_age? || parent_permission
 
     false
   end
