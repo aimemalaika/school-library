5ac82b65a02cb9c79a2ddc0023bed4bfe8f28c06
Aime Malaika <aimemalaika1995@gmail.com>
2022-04-09 09:40:58 +0200

    set up associations

--- main.rb
+++ main.rb
@@ -1,4 +1,7 @@
 require_relative './person'
+require_relative './book'
+require_relative './rental'
+require_relative './student'
 require_relative './capitalize_name'
 require_relative './trim_name'
 
@@ -10,3 +13,17 @@ capitalized_trimmed_person = TrimeName.new(capitalized_person)
 capitalized_trimmed_person.correct_name
 
 puts capitalized_trimmed_person.correct_name
+
+student1 = Person.new(22, 'Malaika')
+student2 = Person.new(27, 'MalaikaAime')
+
+book1 = Book.new('Book1 Title', 'Book1 Author')
+book2 = Book.new('Book2 Title', 'Book2 Author')
+book3 = Book.new('Book3 Title', 'Book3 Author')
+
+rental1 = Rental.new(book1, student1)
+rental2 = Rental.new(book2, student1)
+rental3 = Rental.new(book3, student2)
+
+puts "Total books rented by #{student1.name} = #{student1.rentals.length}"
+puts rental1.show_rental, rental2.show_rental, rental3.show_rental
---
+++ rental.rb
@@ -0,0 +1,17 @@
+class Rental
+  attr_accessor :date
+
+  def initialize(book, person, date = Time.now)
+    @date = date
+
+    @book = book
+    book.rentals.push(self)
+
+    @person = person
+    person.rentals.push(self)
+  end
+
+  def show_rental
+    "#{@person.name} #{@book.title}  #{date}"
+  end
+end
